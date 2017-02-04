787c351acdc17388187f32878619eb92f1181088
Kun Ma <makun211@gmail.com>
2017-02-03 16:06:30 -0800

    Some minor changes

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,8 +10,8 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/regression/**/coverageTest.js',
-        './test/regression/**/searchTest.js'
+        './test/regression/**/coverageTest.js'
+        // './test/regression/**/searchTest.js'
     ],
     suites: {
         login:[],
