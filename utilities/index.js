4638ef9cf297ad732be2e0eeb9ba0c5ef0c88c53
Kun Ma <makun211@gmail.com>
2017-02-06 17:02:07 -0800

    Rewrote the way to construct utilities

--- utilities/index.js
+++ utilities/index.js
@@ -1,20 +1,100 @@
 module.exports = () => {
-    // todo crawl through all directors, compile all classes,
-    // add them to the global namespace as tools
     const fs = require("fs"),
         path = require("path"),
         utilDirectory = __dirname,
         assert = require(`${utilDirectory}/assert-fix`),
-        utilities = require(`${utilDirectory}/util-builder`),
+        async = require("asyncawait/async"),
+        await = require("asyncawait/await")
+    /**
+     * A class to build utilities
+     */
+    class UtilBuilder {
+        /**
+         * @param {object} browser - globle instance from webdriverIO
+         * Here we start our utilities building
+         */
+        constructor (browser) {
+            this.browser = browser,
+            this.utilDirectory = __dirname,
+            this.moduleFiles = [],
+            this.utilities = {},
+            this.verifyUtilDir()
+            return this
+        }
+        /**
+         * Here we verify the utilities directory is valid
+         */
+        verifyUtilDir () {
+            try {
+                fs.statSync(this.utilDirectory).isDirectory()
+            } catch (e) {
+                return
+            }
+            this.findUtilities(this.utilDirectory)
+        }
+        /**
+         * Here we read all utility files under utilities folder
+         */
+        findUtilities (utilDir) {
+            fs.readdirSync(utilDir).forEach(
+                (file) => {
+                    let filePath = `${utilDir}/${file}`
+                    if (fs.statSync(filePath).isDirectory()) {
+                        this.findUtilities(filePath)
+                    } else {
+                        this.moduleFiles.push(filePath)
+                    }
+                }
+            )
+            this.buildUtilities()
+        }
+        /**
+         * Here we build all utilities
+         */
+        buildUtilities () {
+            this.moduleFiles.filter(
+                (file) => {
+                    return file.substr(-10) === "-module.js"
+                }
+            ).map(
+                (module) => {
+                    let temp = require(module)
+                    if (typeof temp === "function") {
+                        temp = require(module)(this.browser)
+                    }
+                    let fileName = module.replace(/^.*[\\\/]/, ""),
+                        moduleName = fileName.split("-").map((fs, index) => {
+                            if (index > 0) {
+                                let char = fs.charAt(0).toUpperCase()
+                                return `${char}${fs.substring(1)}`
+                            } else {
+                                return fs
+                            }
+                        }).join("").split(".js")[0]
+                    this.utilities[moduleName] = temp
+                }
+            )
+            this.assignUtilities()
+        }
+        /**
+         * Here we add all utilities to global space
+         */
+        assignUtilities () {
+            Object.assign(global, this.utilities)
+        }
+    }
+
+    let buildUtil = new UtilBuilder(browser),
         tools = {
             fs,
             path,
             assert,
-            utilDirectory
+            utilDirectory,
+            async,
+            await
         }
     /**
-     * Add all of our tools & utilities to global space for easier test writing
+     * Add all of our tools to global space for easier test writing
      */
-    Object.assign(tools, utilities)
     Object.assign(global, tools)
 }
